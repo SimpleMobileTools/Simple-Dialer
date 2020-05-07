56f53f5659eeeb01e69fabb05c4cbf47f247d8c5
tibbi <tibor@kaputa.sk>
2020-05-07 21:31:41 +0200

    set up the letters sidebar fastscroller

--- app/src/main/kotlin/com/simplemobiletools/dialer/fragments/MyViewPagerFragment.kt
+++ app/src/main/kotlin/com/simplemobiletools/dialer/fragments/MyViewPagerFragment.kt
@@ -3,7 +3,10 @@ package com.simplemobiletools.dialer.fragments
 import android.content.Context
 import android.util.AttributeSet
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.reddit.indicatorfastscroll.FastScrollItemIndicator
 import com.simplemobiletools.commons.adapters.MyRecyclerViewAdapter
+import com.simplemobiletools.commons.extensions.getColorStateList
+import com.simplemobiletools.commons.extensions.getContrastColor
 import com.simplemobiletools.commons.extensions.underlineText
 import com.simplemobiletools.commons.models.SimpleContact
 import com.simplemobiletools.dialer.activities.SimpleActivity
@@ -11,6 +14,8 @@ import com.simplemobiletools.dialer.adapters.ContactsAdapter
 import com.simplemobiletools.dialer.extensions.config
 import com.simplemobiletools.dialer.helpers.Config
 import kotlinx.android.synthetic.main.fragment_letters_layout.view.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 abstract class MyViewPagerFragment(context: Context, attributeSet: AttributeSet) : CoordinatorLayout(context, attributeSet) {
     protected var activity: SimpleActivity? = null
@@ -38,5 +43,22 @@ abstract class MyViewPagerFragment(context: Context, attributeSet: AttributeSet)
         }.apply {
             fragment_list.adapter = this
         }
+
+        letter_fastscroller.textColor = config.textColor.getColorStateList()
+        setupLetterFastscroller(contacts)
+        letter_fastscroller_thumb.setupWithFastScroller(letter_fastscroller)
+        letter_fastscroller_thumb.textColor = config.primaryColor.getContrastColor()
+    }
+
+    private fun setupLetterFastscroller(contacts: ArrayList<SimpleContact>) {
+        letter_fastscroller.setupWithRecyclerView(fragment_list, { position ->
+            try {
+                val name = contacts[position].name
+                val character = if (name.isNotEmpty()) name.substring(0, 1) else ""
+                FastScrollItemIndicator.Text(character.toUpperCase(Locale.getDefault()))
+            } catch (e: Exception) {
+                FastScrollItemIndicator.Text("")
+            }
+        })
     }
 }
