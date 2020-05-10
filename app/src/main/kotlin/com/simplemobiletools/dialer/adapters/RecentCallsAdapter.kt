6a6fbfdcd478771c24f7d3598a7c3766325e8e4b
tibbi <tibor@kaputa.sk>
2020-05-10 23:58:01 +0200

    use proper text colors at the Recents list

--- app/src/main/kotlin/com/simplemobiletools/dialer/adapters/RecentCallsAdapter.kt
+++ app/src/main/kotlin/com/simplemobiletools/dialer/adapters/RecentCallsAdapter.kt
@@ -65,16 +65,19 @@ class RecentCallsAdapter(activity: SimpleActivity, var recentCalls: ArrayList<Re
         view.apply {
             item_recents_name.apply {
                 text = call.name
+                setTextColor(textColor)
                 setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize)
             }
 
             item_recents_date_time.apply {
                 text = call.startTS.formatDateOrTime(context, true)
+                setTextColor(textColor)
                 setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize * 0.8f)
             }
 
             item_recents_duration.apply {
                 text = call.duration.getFormattedDuration()
+                setTextColor(textColor)
                 beVisibleIf(call.type != Calls.MISSED_TYPE && call.type != Calls.REJECTED_TYPE && call.duration > 0)
                 setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize * 0.8f)
             }
