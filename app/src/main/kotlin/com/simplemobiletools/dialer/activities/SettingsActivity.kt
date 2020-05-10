7e7911dd0013f6d9394f22b8df812f024827b881
tibbi <tibor@kaputa.sk>
2020-05-10 21:23:53 +0200

    adding a setting item for managing blocked numbers

--- app/src/main/kotlin/com/simplemobiletools/dialer/activities/SettingsActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/dialer/activities/SettingsActivity.kt
@@ -1,10 +1,15 @@
 package com.simplemobiletools.dialer.activities
 
+import android.annotation.TargetApi
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
+import com.simplemobiletools.commons.activities.ManageBlockedNumbersActivity
 import com.simplemobiletools.commons.dialogs.ChangeDateTimeFormatDialog
 import com.simplemobiletools.commons.extensions.beVisibleIf
 import com.simplemobiletools.commons.extensions.updateTextColors
+import com.simplemobiletools.commons.helpers.isNougatPlus
 import com.simplemobiletools.dialer.R
 import com.simplemobiletools.dialer.extensions.config
 import kotlinx.android.synthetic.main.activity_settings.*
@@ -21,6 +26,7 @@ class SettingsActivity : SimpleActivity() {
 
         setupCustomizeColors()
         setupUseEnglish()
+        setupManageBlockedNumbers()
         setupChangeDateTimeFormat()
         updateTextColors(settings_holder)
         invalidateOptionsMenu()
@@ -44,6 +50,17 @@ class SettingsActivity : SimpleActivity() {
             settings_use_english.toggle()
             config.useEnglish = settings_use_english.isChecked
             System.exit(0)
+        }
+    }
+
+    // support for device-wise blocking came on Android 7, rely only on that
+    @TargetApi(Build.VERSION_CODES.N)
+    private fun setupManageBlockedNumbers() {
+        settings_manage_blocked_numbers_holder.beVisibleIf(isNougatPlus())
+        settings_manage_blocked_numbers_holder.setOnClickListener {
+            Intent(this, ManageBlockedNumbersActivity::class.java).apply {
+                startActivity(this)
+            }
         }
     }
 
