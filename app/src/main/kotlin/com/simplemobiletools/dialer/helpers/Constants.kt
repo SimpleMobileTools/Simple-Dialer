92589167517572a94fd19e361ea0d04690d3d632
tibbi <tibor@kaputa.sk>
2020-05-13 17:25:08 +0200

    moving a string in Commons

--- app/src/main/kotlin/com/simplemobiletools/dialer/helpers/Constants.kt
+++ app/src/main/kotlin/com/simplemobiletools/dialer/helpers/Constants.kt
@@ -10,8 +10,6 @@ const val RECENTS_TAB_MASK = 4
 
 val tabsList = arrayListOf(CONTACTS_TAB_MASK, RECENTS_TAB_MASK)
 
-const val KEY_PHONE = "phone"
-
 private const val PATH = "com.simplemobiletools.dialer.action."
 const val ACCEPT_CALL = PATH + "accept_call"
 const val DECLINE_CALL = PATH + "decline_call"
