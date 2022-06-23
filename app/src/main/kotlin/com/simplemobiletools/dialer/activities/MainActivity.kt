9e614c9e3564fc7cdd894fce544932b63cef8a7d
tibbi <tibor@kaputa.sk>
2022-06-23 22:24:11 +0200

    avoid showing some FAQ items at the fdroid build variant

--- app/src/main/kotlin/com/simplemobiletools/dialer/activities/MainActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/dialer/activities/MainActivity.kt
@@ -469,11 +469,14 @@ class MainActivity : SimpleActivity() {
         val licenses = LICENSE_GLIDE or LICENSE_INDICATOR_FAST_SCROLL
 
         val faqItems = arrayListOf(
-            FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons),
-            FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons),
             FAQItem(R.string.faq_7_title_commons, R.string.faq_7_text_commons),
             FAQItem(R.string.faq_9_title_commons, R.string.faq_9_text_commons)
         )
+
+        if (!resources.getBoolean(R.bool.hide_google_relations)) {
+            faqItems.add(FAQItem(R.string.faq_2_title_commons, R.string.faq_2_text_commons))
+            faqItems.add(FAQItem(R.string.faq_6_title_commons, R.string.faq_6_text_commons))
+        }
 
         startAboutActivity(R.string.app_name, licenses, BuildConfig.VERSION_NAME, faqItems, true)
     }
