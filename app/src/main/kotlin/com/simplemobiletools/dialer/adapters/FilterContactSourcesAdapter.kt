1f3bcf151e1242963e4855f44d8b831104a09a5f
ismailnurudeen <ibrightstar247@gmail.com>
2023-03-13 20:29:35 +0100

    feat: implemented filtering similar to simple contact.

---
+++ app/src/main/kotlin/com/simplemobiletools/dialer/adapters/FilterContactSourcesAdapter.kt
@@ -0,0 +1,78 @@
+package com.simplemobiletools.dialer.adapters
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.simplemobiletools.commons.extensions.getProperBackgroundColor
+import com.simplemobiletools.commons.extensions.getProperPrimaryColor
+import com.simplemobiletools.commons.extensions.getProperTextColor
+import com.simplemobiletools.commons.helpers.SMT_PRIVATE
+import com.simplemobiletools.commons.models.contacts.ContactSource
+import com.simplemobiletools.dialer.R
+import com.simplemobiletools.dialer.activities.SimpleActivity
+import kotlinx.android.synthetic.main.item_filter_contact_source.view.*
+
+class FilterContactSourcesAdapter(
+    val activity: SimpleActivity,
+    private val contactSources: List<ContactSource>,
+    private val displayContactSources: ArrayList<String>
+) : RecyclerView.Adapter<FilterContactSourcesAdapter.ViewHolder>() {
+
+    private val selectedKeys = HashSet<Int>()
+
+    init {
+        contactSources.forEachIndexed { index, contactSource ->
+            if (displayContactSources.contains(contactSource.name)) {
+                selectedKeys.add(contactSource.hashCode())
+            }
+
+            if (contactSource.type == SMT_PRIVATE && displayContactSources.contains(SMT_PRIVATE)) {
+                selectedKeys.add(contactSource.hashCode())
+            }
+        }
+    }
+
+    private fun toggleItemSelection(select: Boolean, contactSource: ContactSource, position: Int) {
+        if (select) {
+            selectedKeys.add(contactSource.hashCode())
+        } else {
+            selectedKeys.remove(contactSource.hashCode())
+        }
+
+        notifyItemChanged(position)
+    }
+
+    fun getSelectedContactSources() = contactSources.filter { selectedKeys.contains(it.hashCode()) }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = activity.layoutInflater.inflate(R.layout.item_filter_contact_source, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val contactSource = contactSources[position]
+        holder.bindView(contactSource)
+    }
+
+    override fun getItemCount() = contactSources.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bindView(contactSource: ContactSource): View {
+            val isSelected = selectedKeys.contains(contactSource.hashCode())
+            itemView.apply {
+                filter_contact_source_checkbox.isChecked = isSelected
+                filter_contact_source_checkbox.setColors(activity.getProperTextColor(), activity.getProperPrimaryColor(), activity.getProperBackgroundColor())
+                val countText = if (contactSource.count >= 0) " (${contactSource.count})" else ""
+                val displayName = "${contactSource.publicName}$countText"
+                filter_contact_source_checkbox.text = displayName
+                filter_contact_source_holder.setOnClickListener { viewClicked(!isSelected, contactSource) }
+            }
+
+            return itemView
+        }
+
+        private fun viewClicked(select: Boolean, contactSource: ContactSource) {
+            toggleItemSelection(select, contactSource, adapterPosition)
+        }
+    }
+}
