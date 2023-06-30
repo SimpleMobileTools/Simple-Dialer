a23dcd53024e05bc268c55ae4585f58552d813c6
tibbi <tibor@kaputa.sk>
2023-06-30 14:25:56 +0200

    fixing some margins

--- app/src/main/kotlin/com/simplemobiletools/dialer/adapters/RecentCallsAdapter.kt
+++ app/src/main/kotlin/com/simplemobiletools/dialer/adapters/RecentCallsAdapter.kt
@@ -278,7 +278,6 @@ class RecentCallsAdapter(
     private fun getSelectedPhoneNumber() = getSelectedItems().firstOrNull()?.phoneNumber
 
     private fun setupView(view: View, call: RecentCall) {
-        val shouldShowOverflowMenu = showOverflowMenu && !call.isUnknownNumber
         view.apply {
             item_recents_holder.isSelected = selectedKeys.contains(call.id)
             val name = findContactByCall(call)?.getNameToDisplay() ?: call.name
@@ -317,7 +316,7 @@ class RecentCallsAdapter(
                 setTextColor(textColor)
                 beVisibleIf(call.type != Calls.MISSED_TYPE && call.type != Calls.REJECTED_TYPE && call.duration > 0)
                 setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize * 0.8f)
-                if (!shouldShowOverflowMenu) {
+                if (!showOverflowMenu) {
                     item_recents_duration.setPadding(0, 0, durationPadding, 0)
                 }
             }
@@ -340,6 +339,7 @@ class RecentCallsAdapter(
 
             item_recents_type.setImageDrawable(drawable)
 
+            overflow_menu_icon.beVisibleIf(showOverflowMenu)
             overflow_menu_icon.drawable.apply {
                 mutate()
                 setTint(activity.getProperTextColor())
