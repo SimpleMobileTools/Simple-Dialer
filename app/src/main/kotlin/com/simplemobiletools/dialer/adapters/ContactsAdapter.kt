ec57e45770a73fbc457db9407def3c185368b23f
merkost <konstantinlikes@gmail.com>
2023-07-01 12:02:59 +1000

    Fixed a bug - for some reason FrameLayout was casted to Constraint layout

--- app/src/main/kotlin/com/simplemobiletools/dialer/adapters/ContactsAdapter.kt
+++ app/src/main/kotlin/com/simplemobiletools/dialer/adapters/ContactsAdapter.kt
@@ -11,9 +11,9 @@ import android.view.Menu
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
@@ -271,7 +271,7 @@ class ContactsAdapter(
     private fun setupView(view: View, contact: Contact, holder: ViewHolder) {
         view.apply {
             setupViewBackground(activity)
-            findViewById<ConstraintLayout>(R.id.item_contact_frame).isSelected = selectedKeys.contains(contact.rawId)
+            findViewById<FrameLayout>(R.id.item_contact_frame).isSelected = selectedKeys.contains(contact.rawId)
             findViewById<TextView>(R.id.item_contact_name).apply {
                 setTextColor(textColor)
                 setTextSize(TypedValue.COMPLEX_UNIT_PX, fontSize)
