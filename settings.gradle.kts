3959383322879f6f5020c2b1401e7111af1f7656
fatih ergin <erginfatih@yahoo.com>
2023-09-06 23:44:10 +0300

    migrate to gradle kts and version catalog

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+rootProject.name = "Simple-Dialer"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { setUrl("https://jitpack.io") }
+    }
+}
+include(":app")
